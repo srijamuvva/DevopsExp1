d6138dd84e6aaa7df6cdf4a4374cf966783519de
srija <147132543+srijamuvva@users.noreply.github.com>
2025-01-02 12:05:19 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+class Multiplication{
+	public static void main(String [] args){
+		int a=5;
+		int b=5;
+		System.out.println("Multiplication of two numbers:");
+		System.out.println(a+"*"+b+"="+a*b);
+	}
+}
